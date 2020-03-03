284ea94a9e4718b576ad618b3012c90a87331300
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-03-03 14:33:59 -0700

    made an infinite loop and broke need to rework

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ const App = () => {
       localStorage.setItem(localStorage, newItem);
       setLocal(newItem);
     }
+    return [local, setLocal];
   }
+  useLocalState();
+  const [value, setValue] = useState("value");
   return (
     <div className="App">
       <h1>State persisting into Local storage</h1>
+      <p>ToDo: {value}</p>
+      <button onClick={() => setValue("code")}>Code</button>
     </div>
   );
 };
